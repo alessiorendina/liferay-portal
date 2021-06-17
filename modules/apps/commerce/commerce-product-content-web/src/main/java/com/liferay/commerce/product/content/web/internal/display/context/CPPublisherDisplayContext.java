51f564377403ee22ad6ef54adc64d420abf50e05
Alessio Rendina <asilcomando@gmail.com>
2021-06-17 11:02:13 +0200

    COMMERCE-6785 Fixed product publisher URL

--- modules/apps/commerce/commerce-product-content-web/src/main/java/com/liferay/commerce/product/content/web/internal/display/context/CPPublisherDisplayContext.java
+++ modules/apps/commerce/commerce-product-content-web/src/main/java/com/liferay/commerce/product/content/web/internal/display/context/CPPublisherDisplayContext.java
@@ -16,24 +16,41 @@ package com.liferay.commerce.product.content.web.internal.display.context;
 
 import com.liferay.commerce.product.catalog.CPCatalogEntry;
 import com.liferay.commerce.product.catalog.CPQuery;
+import com.liferay.commerce.product.constants.CPPortletKeys;
+import com.liferay.commerce.product.constants.CPWebKeys;
 import com.liferay.commerce.product.content.render.list.CPContentListRendererRegistry;
 import com.liferay.commerce.product.content.render.list.entry.CPContentListEntryRendererRegistry;
 import com.liferay.commerce.product.content.web.internal.util.CPPublisherWebHelper;
 import com.liferay.commerce.product.data.source.CPDataSource;
 import com.liferay.commerce.product.data.source.CPDataSourceRegistry;
 import com.liferay.commerce.product.data.source.CPDataSourceResult;
+import com.liferay.commerce.product.model.CProduct;
 import com.liferay.commerce.product.type.CPType;
 import com.liferay.commerce.product.type.CPTypeServicesTracker;
+import com.liferay.commerce.product.url.CPFriendlyURL;
 import com.liferay.commerce.product.util.CPDefinitionHelper;
+import com.liferay.friendly.url.model.FriendlyURLEntry;
+import com.liferay.friendly.url.service.FriendlyURLEntryLocalService;
 import com.liferay.petra.portlet.url.builder.PortletURLBuilder;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.search.SearchContainer;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutTypePortlet;
+import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.portlet.PortletURLWrapper;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.io.Serializable;
@@ -58,9 +75,10 @@ public class CPPublisherDisplayContext extends BaseCPPublisherDisplayContext {
 			CPContentListEntryRendererRegistry contentListEntryRendererRegistry,
 			CPContentListRendererRegistry cpContentListRendererRegistry,
 			CPDataSourceRegistry cpDataSourceRegistry,
-			CPDefinitionHelper cpDefinitionHelper,
+			CPDefinitionHelper cpDefinitionHelper, CPFriendlyURL cpFriendlyURL,
 			CPPublisherWebHelper cpPublisherWebHelper,
 			CPTypeServicesTracker cpTypeServicesTracker,
+			FriendlyURLEntryLocalService friendlyURLEntryLocalService,
 			HttpServletRequest httpServletRequest)
 		throws PortalException {
 
@@ -70,6 +88,13 @@ public class CPPublisherDisplayContext extends BaseCPPublisherDisplayContext {
 
 		_cpDataSourceRegistry = cpDataSourceRegistry;
 		_cpDefinitionHelper = cpDefinitionHelper;
+		_cpFriendlyURL = cpFriendlyURL;
+		_friendlyURLEntryLocalService = friendlyURLEntryLocalService;
+
+		_cProductId = _getCProductId();
+
+		_delta = ParamUtil.getInteger(
+			httpServletRequest, "delta", getPaginationDelta());
 	}
 
 	public Map<String, String> getCPContentListEntryRendererKeys() {
@@ -131,14 +156,46 @@ public class CPPublisherDisplayContext extends BaseCPPublisherDisplayContext {
 		LiferayPortletResponse liferayPortletResponse =
 			cpContentRequestHelper.getLiferayPortletResponse();
 
-		String delta = ParamUtil.getString(
-			cpContentRequestHelper.getRequest(), "delta",
-			String.valueOf(getPaginationDelta()));
+		if (_hasCPContentPortlet()) {
+			PortletURL portletURL = new PortletURLWrapper(
+				liferayPortletResponse.createRenderURL()) {
+
+				@Override
+				public String toString() {
+					HttpServletRequest originalHttpServletRequest =
+						PortalUtil.getOriginalServletRequest(
+							cpContentRequestHelper.getRequest());
+
+					ThemeDisplay themeDisplay =
+						(ThemeDisplay)originalHttpServletRequest.getAttribute(
+							WebKeys.THEME_DISPLAY);
+
+					Layout layout = themeDisplay.getLayout();
+
+					String layoutFriendlyURL = layout.getFriendlyURL(
+						themeDisplay.getLocale());
+
+					String productURLSeparator =
+						_cpFriendlyURL.getProductURLSeparator(
+							themeDisplay.getCompanyId());
+
+					return StringUtil.replace(
+						super.toString(), layoutFriendlyURL,
+						productURLSeparator + _getFriendlyURL());
+				}
+
+			};
+
+			portletURL.setParameter("cProductId", String.valueOf(_cProductId));
+			portletURL.setParameter("delta", String.valueOf(_delta));
+
+			return portletURL;
+		}
 
 		return PortletURLBuilder.createRenderURL(
 			liferayPortletResponse
 		).setParameter(
-			"delta", delta
+			"delta", String.valueOf(_delta)
 		).build();
 	}
 
@@ -153,8 +210,7 @@ public class CPPublisherDisplayContext extends BaseCPPublisherDisplayContext {
 			cpContentRequestHelper.getLiferayPortletRequest(), getPortletURL(),
 			null, "there-are-no-products");
 
-		_searchContainer.setDelta(
-			cpPublisherPortletInstanceConfiguration.paginationDelta());
+		_searchContainer.setDelta(_delta);
 
 		CPDataSourceResult cpDataSourceResult = getCPDataSourceResult();
 
@@ -165,6 +221,27 @@ public class CPPublisherDisplayContext extends BaseCPPublisherDisplayContext {
 		}
 
 		return _searchContainer;
+	}
+
+	private long _getCProductId() {
+		HttpServletRequest httpServletRequest =
+			cpContentRequestHelper.getRequest();
+
+		long cProductId = ParamUtil.getLong(httpServletRequest, "cProductId");
+
+		if (cProductId > 0) {
+			return cProductId;
+		}
+
+		CPCatalogEntry cpCatalogEntry =
+			(CPCatalogEntry)httpServletRequest.getAttribute(
+				CPWebKeys.CP_CATALOG_ENTRY);
+
+		if (cpCatalogEntry != null) {
+			cProductId = cpCatalogEntry.getCProductId();
+		}
+
+		return cProductId;
 	}
 
 	private CPDataSourceResult _getDynamicCPDataSourceResult(int start, int end)
@@ -197,8 +274,60 @@ public class CPPublisherDisplayContext extends BaseCPPublisherDisplayContext {
 			start, end);
 	}
 
+	private String _getFriendlyURL() {
+		FriendlyURLEntry friendlyURLEntry = null;
+
+		try {
+			friendlyURLEntry =
+				_friendlyURLEntryLocalService.getMainFriendlyURLEntry(
+					PortalUtil.getClassNameId(CProduct.class), _cProductId);
+		}
+		catch (Exception exception) {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					"No friendly URL found for " + _getCProductId(), exception);
+			}
+
+			return StringPool.BLANK;
+		}
+
+		ThemeDisplay themeDisplay = cpContentRequestHelper.getThemeDisplay();
+
+		return friendlyURLEntry.getUrlTitle(themeDisplay.getLanguageId());
+	}
+
+	private boolean _hasCPContentPortlet() {
+		boolean hasCPContentPortlet = false;
+
+		Layout layout = cpContentRequestHelper.getLayout();
+
+		LayoutTypePortlet layoutTypePortlet =
+			(LayoutTypePortlet)layout.getLayoutType();
+
+		List<Portlet> portlets = layoutTypePortlet.getAllPortlets();
+
+		for (Portlet portlet : portlets) {
+			String portletId = portlet.getPortletId();
+
+			if (portletId.startsWith(CPPortletKeys.CP_CONTENT_WEB)) {
+				hasCPContentPortlet = true;
+
+				break;
+			}
+		}
+
+		return hasCPContentPortlet;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CPPublisherDisplayContext.class);
+
 	private final CPDataSourceRegistry _cpDataSourceRegistry;
 	private final CPDefinitionHelper _cpDefinitionHelper;
+	private final CPFriendlyURL _cpFriendlyURL;
+	private final long _cProductId;
+	private final int _delta;
+	private final FriendlyURLEntryLocalService _friendlyURLEntryLocalService;
 	private SearchContainer<CPCatalogEntry> _searchContainer;
 
 }
