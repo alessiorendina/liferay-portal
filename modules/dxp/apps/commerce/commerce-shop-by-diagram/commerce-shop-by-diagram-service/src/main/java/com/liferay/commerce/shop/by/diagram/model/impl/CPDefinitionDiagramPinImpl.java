009b38e5f05418fb224149c6784afe27ac6e0e60
Alessio Rendina <asilcomando@gmail.com>
2021-07-08 16:14:23 +0200

    COMMERCE-5661 Added commerce product definition diagram pin implementation

--- modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-service/src/main/java/com/liferay/commerce/shop/by/diagram/model/impl/CPDefinitionDiagramPinImpl.java
+++ modules/dxp/apps/commerce/commerce-shop-by-diagram/commerce-shop-by-diagram-service/src/main/java/com/liferay/commerce/shop/by/diagram/model/impl/CPDefinitionDiagramPinImpl.java
@@ -14,23 +14,23 @@
 
 package com.liferay.commerce.shop.by.diagram.model.impl;
 
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.service.CPDefinitionLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+
 /**
- * The extended model implementation for the CPDefinitionDiagramPin service. Represents a row in the &quot;CPDefinitionDiagramPin&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.commerce.shop.by.diagram.model.CPDefinitionDiagramPin</code> interface.
- * </p>
- *
  * @author Andrea Sbarra
+ * @author Alessio Antonio Rendina
  */
 public class CPDefinitionDiagramPinImpl extends CPDefinitionDiagramPinBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a cp definition diagram pin model instance should use the {@link com.liferay.commerce.shop.by.diagram.model.CPDefinitionDiagramPin} interface instead.
-	 */
 	public CPDefinitionDiagramPinImpl() {
+	}
+
+	@Override
+	public CPDefinition getCPDefinition() throws PortalException {
+		return CPDefinitionLocalServiceUtil.getCPDefinition(
+			getCPDefinitionId());
 	}
 
 }
