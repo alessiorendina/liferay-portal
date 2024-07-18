25c18d5eba089b9b4ef0e00a3b8ce60256f47342
Alessio Rendina <asilcomando@gmail.com>
2024-07-18 16:57:04 +0200

    LPD-30188 Moved poshi test to playwright

--- modules/test/playwright/pages/commerce/commerce-product-content-web/productDetailsPage.ts
+++ modules/test/playwright/pages/commerce/commerce-product-content-web/productDetailsPage.ts
@@ -197,7 +197,7 @@ export class ProductDetailsPage {
 		await this.frameSubmitSpecification.click();
 	}
 
-	async changeValueInProductSPecification(
+	async changeValueInProductSpecification(
 		chooseAddOrCreate: string,
 		specificationValue: string
 	) {
