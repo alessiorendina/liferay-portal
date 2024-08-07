604b1ff82b0e9667ae322d700a15942182b502f5
Carolina Barbosa <carolina.barbosa@liferay.com>
2024-08-07 16:45:49 -0300

    LPD-33056 Add playwright test

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -12,6 +12,7 @@ import {config as analyticsWebConfig} from './tests/analytics-web/config';
 import {config as announcementsWebConfig} from './tests/announcements-web/config';
 import {config as batchPlannerConfig} from './tests/batch-planner/config';
 import {config as blogsWebConfig} from './tests/blogs-web/config';
+import {config as calendarWebConfig} from './tests/calendar-web/config';
 import {config as changeTrackingWebConfig} from './tests/change-tracking-web/config';
 import {config as clientExtensionWebConfig} from './tests/client-extension-web/config';
 import {config as commerceConfig} from './tests/commerce/config';
@@ -78,6 +79,7 @@ export default defineConfig({
 		announcementsWebConfig,
 		batchPlannerConfig,
 		blogsWebConfig,
+		calendarWebConfig,
 		cookiesBannerWebConfig,
 		changeTrackingWebConfig,
 		clientExtensionWebConfig,
