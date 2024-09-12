1e9c0800426980d5e1b606bbd847ad56467755e8
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-09-12 07:45:37 +0200

    LPD-36078 Rename

--- modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/internal/search/spi/model/index/contributor/test/LayoutModelDocumentContributorTest.java
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/internal/search/spi/model/index/contributor/test/LayoutModelDocumentContributorTest.java
@@ -124,7 +124,7 @@ public class LayoutModelDocumentContributorTest {
 	}
 
 	@Test
-	public void testContentLayoutSummary() throws Exception {
+	public void testGetContentLayoutSummary() throws Exception {
 		String defaultLocaleElementText = RandomTestUtil.randomString();
 		String spanishElementText = RandomTestUtil.randomString();
 
