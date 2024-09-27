ebe977767127dbb5d5cb30286cf25bf5f75ac620
Brian Chan <brian.chan@liferay.com>
2024-09-27 15:58:09 -0300

    LPD-35277 Rename

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectRelationshipLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectRelationshipLocalServiceTest.java
@@ -504,7 +504,7 @@ public class ObjectRelationshipLocalServiceTest {
 	}
 
 	@Test
-	public void testRegisterObjectRelationshipsRelatedInfoCollectionProviders()
+	public void testRegisterObjectRelationshipsRelatedInfoItemCollectionProviders()
 		throws Exception {
 
 		ObjectDefinition objectDefinition1 =
