6007ab2234afb7b5388720fb4a6e36fea344064b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-09-27 07:15:00 +0200

    LPD-37554 The default value should be the site default, since the themeDisplay.getLanguageId() has the current user language id

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/manager/FragmentEntryLinkManager.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/manager/FragmentEntryLinkManager.java
@@ -354,7 +354,8 @@ public class FragmentEntryLinkManager {
 			_getInfoForm(fragmentEntryLink, layoutStructure));
 
 		String languageId = ParamUtil.getString(
-			httpServletRequest, "languageId", themeDisplay.getLanguageId());
+			httpServletRequest, "languageId",
+			LocaleUtil.toLanguageId(themeDisplay.getSiteDefaultLocale()));
 
 		defaultFragmentRendererContext.setLocale(
 			LocaleUtil.fromLanguageId(languageId));
