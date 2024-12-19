1edca95dc2cf86f4cdcb842dc7c876302ba2f8b7
Alessio Rendina <asilcomando@gmail.com>
2024-12-19 08:57:40 +0100

    LPD-37886 SF

--- modules/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/exception/RequiredCPConfigurationEntryException.java
+++ modules/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/exception/RequiredCPConfigurationEntryException.java
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 package com.liferay.commerce.product.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
@@ -18,7 +19,9 @@ public class RequiredCPConfigurationEntryException extends PortalException {
 		super(msg);
 	}
 
-	public RequiredCPConfigurationEntryException(String msg, Throwable throwable) {
+	public RequiredCPConfigurationEntryException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
